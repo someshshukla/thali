8337492e8dfa12abe383b25daf97408ce9d52e93
Somesh Shukla <somesh13shukla@gmail.com>
2023-10-04 22:16:04 +0530

    carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,29 +1,85 @@
-import React from 'react'
+import React from "react";
 
 function Carousel() {
   return (
-    <div><div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://source.unsplash.com/random/900x700/?burger" class="d-block w-100" alt="..."/>
-      </div>
-      <div class="carousel-item">
-        <img src="https://source.unsplash.com/random/900x700/?noodles" class="d-block w-100" alt="..."/>
-      </div>
-      <div class="carousel-item">
-        <img src="https://source.unsplash.com/random/900x700/?biryani" class="d-block w-100" alt="..."/>
+    <div>
+    <div>
+      <div
+        id="carouselExampleFade"
+        className="carousel slide carousel-fade"
+        data-bs-ride="carousel" style={{objectFit: "contain !important"}}
+      >
+        <div className="carousel-inner">
+          <div className="carousel-caption" style={{zIndex:"10"}}>
+            <nav className="navbar navbar-light bg-light">
+              <div className="container-fluid">
+                <form className="d-flex">
+                  <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <button className="btn btn-outline-success text-white bg-white" type="submit">
+                    Search
+                  </button>
+                </form>
+              </div>
+            </nav>
+          </div>
+          <div className="carousel-item active">
+            <img
+              src="https://source.unsplash.com/random/900x700/?indianfood"
+              className="d-block w-100"
+              style={{ filter: "brightness(30%)" }}
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/900x700/?chinesefood"
+              className="d-block w-100"
+              style={{ filter: "brightness(30%)" }}
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://source.unsplash.com/random/900x700/?southindianfood"
+              className="d-block w-100"
+              style={{ filter: "brightness(30%)" }}
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleFade"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div></div>
-  )
+    </div>
+  );
 }
 
-export default Carousel
+export default Carousel;
